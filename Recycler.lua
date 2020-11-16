45df6c2842d47d44aa6238f173cdcb4f818b4f95
Jeffrey Skinner <17016344+blocksrocks@users.noreply.github.com>
2020-11-16 15:27:22 -0800

    Update Recycler.lua

--- Recycler.lua
+++ Recycler.lua
@@ -2,36 +2,51 @@ local Recycler = {}
 Recycler.__index = Recycler
 
 function Recycler.new(instance)
-	local self = {}
+	local self = setmetatable({}, Recycler)
 	
-	self.instance = instance:Clone()
-	self.storage  = {}
-	self.total    = 0
+	self._instance = instance:Clone()
+	self._storage = {}
+	self._total = 0
 	
-	return setmetatable(self, Recycler)
+	return self
 end
 
-function Recycler:fetch(parent)
-	self.total = self.total + 1
+function Recycler:Fetch(parent)
+	self._total = self._total + 1
 	
-	if self.total <= #self.storage then
-		return self.storage[self.total]
+	if self._total <= #self._storage then
+		return self._storage[self._total]
 	else
-		local instance = self.instance:Clone()
+		local instance = self._instance:Clone()
 		instance.Parent = parent
 		
-		self.storage[self.total] = instance
+		self._storage[self._total] = instance
 		
 		return instance
 	end
 end
 
-function Recycler:update()
-	for index = self.total + 1, #self.storage do
-		self.storage[index]:Destroy()
+function Recycler:Update()
+	for index = self._total + 1, #self._storage do
+		self._storage[index]:Destroy()
+		self._storage[index] = nil
 	end
 	
-	self.total = 0
+	self._total = 0
+end
+
+function Recycler:Destroy()
+	self._instance:Destroy()
+	self._instance = nil
+	
+	for index = 1, #self._storage do
+		self._storage[index]:Destroy()
+		self._storage[index] = nil
+	end
+	
+	self._total = nil
+	
+	setmetatable(self, nil)
 end
 
 return Recycler
